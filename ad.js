3dad02d51973f63a344a9b0986df212eccb4c545
muta16 <57425699+muta16@users.noreply.github.com>
2023-04-20 18:52:50 +0800

    Update ad.js

--- ad.js
+++ ad.js
@@ -11,8 +11,6 @@ if (url.indexOf("missav") != -1) {
     <link rel="stylesheet" href="https://limbopro.com/CSS/Adblock4limbo.user.css" type="text/css" />\
     <script type="text/javascript" async="async" src="https://limbopro.com/Adguard/Adblock4limbo.user.js"></script>'
     console.log(`oo:${$response.body}`)
-    let data = JSON.parse($response.body)
-    console.log(`json:${data}`)
     let body = $response.body.replace(reg, str).replace(window_open_reg, window_open_str);
     let headers = $response.headers;
     headers['Content-Security-Policy'] = "child-src	'self'";
