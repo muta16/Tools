c7b289e90fc9e546e8ae60bd240d3d639a05eda8
muta16 <57425699+muta16@users.noreply.github.com>
2023-06-30 16:17:01 +0800

    Update loon-baidu2.js

--- loon-baidu2.js
+++ loon-baidu2.js
@@ -67,6 +67,6 @@ function _writeHttpHeader() {
   let conPort = $session.conPort;
   let verify = createVerify(conHost);
 
-  var header = `CONNECT ${conHost}:${conPort}@collab-ws.dingtalk.com HTTP/1.1\r\nHost: @collab-ws.dingtalk.com\r\nX-T5-Auth: ${verify}\r\nProxy-Connection: keep-alive\r\n\r\n`;
+  var header = `CONNECT ${conHost}:${conPort} HTTP/1.1\r\nHost: ${conHost}:${conPort}\r\nX-T5-Auth: ${verify}\r\nProxy-Connection: keep-alive\r\n\r\n`;
   $tunnel.write($session, header);
 }
